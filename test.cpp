e94c5a8911acc15e917ee736398a76bb257ecc46
1412kaito <ivangoivan@gmail.com>
2018-02-07 19:49:49 +0700

    Update test.cpp

--- test.cpp
+++ test.cpp
@@ -6,11 +6,11 @@
 
 using namespace std;
 
-void cetak(int arr[], int n){
+void cetak(ll arr[], int n){
     for (int i=0; i<n; ++i){
-		printf("%d ", arr[i]);
-	}
-	cout << endl;
+    printf("%lld ", arr[i]);
+  }
+  cout << endl;
 }
 
 int digitke (int berapa, int angkanya){
@@ -19,44 +19,44 @@ int digitke (int berapa, int angkanya){
     }
     else{
         berapa++;
-        return (angkanya / (int)(pow(10,(berapa-1))+0.01)) % 10;
+        return (angkanya / (int)(pow(10,(berapa-1))+0.01) % 10);
     }
 }
 
 int main(){
     int n, digit;
-	cin >> n;
-	int arr[n];
-	for (int i=0; i<n; ++i){
-		cin >> arr[i];
-	}
-	int maks = arr[0];
-    for (ll i=1; i<n; ++i){
+  cin >> n;
+  ll arr[n];
+  for (int i=0; i<n; ++i){
+    cin >> arr[i];
+  }
+  int maks = arr[0];
+    for (int i=1; i<n; ++i){
         if (maks < arr[i])
             maks = arr[i];
     }
-	digit = 0;
-	cetak (arr, n);
-	while (maks){
+  digit = 0;
+  cetak (arr, n);
+  while (maks){
         maks /= 10;
         ++digit;
     }
 
     vector <int> temp;
-    for (ll i=0; i<digit; ++i){
+    for (int i=0; i<digit; ++i){
         temp.clear();
-        for (ll sek=0; sek<10; ++sek){
-            for (ll j=0; j<n; ++j){
+        for (int sek=0; sek<10; ++sek){
+            for (int j=0; j<n; ++j){
                 if ( digitke(i, arr[j]) == sek ){
                     temp.push_back(arr[j]);
                     cout << arr[j] << ' ';
                 }
             }
         }
-        for (ll j=0; j<temp.size(); ++j){
+        for (int j=0; j<temp.size(); ++j){
             arr[j] = temp[j];
         }
         cout << endl;
     }
-	return 0;
+  return 0;
 }
